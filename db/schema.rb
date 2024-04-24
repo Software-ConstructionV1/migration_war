09d25eb92ca2e214f9d9f28cabf90a9e9f8cc93b
ayamaher205 <aya.maher.m7md@gmail.com>
2024-04-25 00:35:50 +0200

    feat:add aya column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_220623) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_223419) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_220623) do
     t.datetime "updated_at", null: false
     t.string "ahmed_khaled"
     t.string "hadeer"
+    t.string "aya"
   end
 
 end
