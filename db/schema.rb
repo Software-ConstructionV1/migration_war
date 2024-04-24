b7ce2c0f68102f6840382bbf67ae40ab53ac8c2e
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-25 00:07:20 +0200

    add hadeer column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_082121) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_220623) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "hadeer"
   end
 
 end
