3f83d2638f3f2878927ca0105ca5752a421ac330
EngAlaaKamal <147073553+EngAlaaKamal@users.noreply.github.com>
2024-04-25 12:48:17 +0000

    add alaa kamal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_110321) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_124609) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_110321) do
     t.string "abdelrahman"
     t.string "Ayman"
     t.string "fatma_nasser"
+    t.string "Alaa_Kamal_eldin"
   end
 
 end
