4ff5758b776c504f0fdf73bfe3bcf8c364748416
shimaa fathi <shimaafathi126@gmail.com>
2024-04-25 07:46:43 +0000

    add-ShimaaColumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_073258) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_074624) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_073258) do
     t.string "OmniaMohamed"
     t.string "alaa_sherif"
     t.string "Esraa"
+    t.string "Shimaa_Fathi"
   end
 
 end
