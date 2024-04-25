c5ec50ac2f12347978f1b2d24bf1bf1b1c82f6dc
ahmednasserhu <ahmedd.nasser.hussein@gmail.com>
2024-04-25 02:28:46 +0200

    Nasser column is added 👾️

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_000642) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_002718) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_000642) do
     t.string "aya"
     t.string "Nouran"
     t.string "mostafa"
+    t.string "nasser"
   end
 
 end
