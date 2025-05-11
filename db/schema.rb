e14d122100eb14d8a33be05acf5e7c3ac9b5db13
Monica <monica@DESKTOP-2C083I0>
2025-05-11 16:55:30 +0300

    Add monica column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_11_122410) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_11_135438) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_11_122410) do
     t.string "salma"
     t.string "SaRa"
     t.string "esraafoda"
+    t.string "monica"
   end
 
 end
