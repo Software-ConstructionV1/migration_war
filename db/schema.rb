482bcdbd3105bc304c5bdd0a8b246854e5e14057
marriaammm <mariamz@std.mans.edu.eg>
2025-05-11 18:02:14 +0300

    remove esraafoda column from posts 😛

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_11_145743) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_11_150048) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_11_145743) do
     t.string "Alaa_Kamal_eldin"
     t.string "salma"
     t.string "SaRa"
-    t.string "esraafoda"
     t.string "mariam"
   end
 
