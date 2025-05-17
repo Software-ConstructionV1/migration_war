e8d0f7e52630ab97cbed43a21e5d76e42289a253
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-05-17 14:50:28 +0300

    Add basmala_abdullah column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_17_103603) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_17_114804) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_17_103603) do
     t.string "salma"
     t.string "SaRa"
     t.string "mariam"
+    t.string "youssef_medhat"
+    t.string "basmala_abdullah"
   end
 
   create_table "students", force: :cascade do |t|
