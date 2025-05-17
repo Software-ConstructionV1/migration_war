acdfe38224d31db3402435859e93442b0ed50df9
Aya osama <ao0010512@gmail.com>
2025-05-17 13:27:27 +0300

    Add Table Students &  Add grade column to Students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_13_095038) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_17_102439) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -43,6 +43,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_13_095038) do
     t.string "salma"
     t.string "SaRa"
     t.string "mariam"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "grade"
   end
 
 end
