ba87a8f97270b3b259332189e4eebd0ed48f3f5a
NouranMohamedFouad <nouranloly@gmail.com>
2025-05-17 20:43:01 +0300

    Rename names

--- db/migrate/20250517170322_rename_rahma_edited_by_nouran.rb
+++ db/migrate/20250517170322_rename_rahma_edited_by_nouran.rb
@@ -1,7 +1,7 @@
 class RenameRahmaEditedByNouran < ActiveRecord::Migration[7.1]
   def change
     if column_exists?(:posts, :nouran_mohamed)
-      rename_column :posts, :nouran_mohamed, :rahma_roro_nouran
+      rename_column :posts, :nouran_mohamed, :rahma_roro_nouran 
     else
       puts "Skipping rename from :nouran_mohamed to :rahma_roro_nouran - source column does not exist."
     end
