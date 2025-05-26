4bc73ba273b0a3bec4fc07e87b6318fe75973f27
mostafa99hosny <m.hosney33@gmail.com>
2025-05-26 15:08:30 +0300

    add column mostafa_hosny to table posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_26_110737) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_26_120656) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_110737) do
     t.string "mohammed_amr"
     t.string "Arwa"
     t.string "nada_salah"
+    t.string "mostafa_hosny"
   end
 
   create_table "students", force: :cascade do |t|
