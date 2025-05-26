6f70a85d0e78a0bb9abfcb8d6a9fa836b8513f74
MohamedAliAmean <mdiab0109666@gmail.com>
2025-05-26 14:26:44 +0300

    diab/lab_three_a: Add Post CRUD, User model with validations, address column, and DB constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_18_194809) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_26_112232) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -67,6 +67,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_18_194809) do
     t.string "level"
     t.string "rahma_edited"
     t.string "zezo"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
