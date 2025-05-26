0a54e909fd1dedf2269c77ed87471171f5a61a18
AbdAllahSayed11 <abdallah2001.sayed@gmail.com>
2025-05-26 15:44:02 +0300

    add column Abdallah Sayed to table posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_26_120656) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_26_123945) do
   create_table "posts", force: :cascade do |t|
     t.string "islam"
     t.string "omar"
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_120656) do
     t.string "Arwa"
     t.string "nada_salah"
     t.string "mostafa_hosny"
+    t.string "Abdallah_Sayed"
   end
 
   create_table "students", force: :cascade do |t|
