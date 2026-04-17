69b6ef1b628c67a5213b951abe2f839c1b009231
Abdallah Habsa <abdallah7absa@gmail.com>
2026-04-17 16:29:18 +0200

    added habsa again

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2026_04_17_141238) do
-
+ActiveRecord::Schema[7.1].define(version: 2026_04_17_142011) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
@@ -79,6 +77,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_17_141238) do
     t.string "mahmoud_halim"
     t.string "seif_name"
     t.string "Alaa_anwar"
+    t.string "abdallah_ahmed_habsa"
     t.string "youssefMohamed"
     t.string "masnour"
     t.string "abdallah_ehab"
