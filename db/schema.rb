8c526c1c73686c9423c7252e7215339ecf044fa4
robaa12 <arobaa23@gmail.com>
2026-04-17 21:27:58 +0200

    rename column seif_name to seif_name_robaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_17_192257) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_17_192758) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_17_192257) do
     t.string "ayaayman_Gamgoum"
     t.string "yasminzin_Gamgoum"
     t.string "mahmoud_halim"
-    t.string "seif_name"
+    t.string "seif_name_robaa"
     t.string "Alaa_anwar"
     t.string "abdallah_ahmed_habsa"
     t.string "youssefMohamed"
