6d52819fc725c6fc395b59658fa1e4d6605ca90f
robaa12 <arobaa23@gmail.com>
2026-04-17 21:31:18 +0200

    rename column Alaa_anwar to Alaa_anwar_robaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_17_193106) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_17_193118) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
--- war.sh
+++ war.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TABLE="posts"
-COL="mahmoud_halim"   # change this to whatever you want to test
+COL="Alaa_anwar"   # change this to whatever you want to test
 NEW_COL="${COL}_robaa"
 
 MIGRATION_NAME="rename_${COL}_to_${NEW_COL}"
