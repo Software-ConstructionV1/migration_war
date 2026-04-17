d701028962b833d70544dc3d122a37db558592f9
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-04-17 15:52:52 +0200

    add schema.rb after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_17_122557) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_17_130621) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_17_122557) do
     t.string "ayaayman_Gamgoum"
     t.string "yasminzin_Gamgoum"
     t.string "mahmoud_halim"
+    t.string "seif_name"
+    t.string "Alaa_anwar"
   end
 
   create_table "students", force: :cascade do |t|
