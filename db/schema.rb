8629c21b0ada8c79b65fc25ffcf5b399dcdf8b75
robaa12 <arobaa23@gmail.com>
2026-04-17 21:28:23 +0200

    rename column masnour to masnour_robaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_17_192758) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_17_192822) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_17_192758) do
     t.string "Alaa_anwar"
     t.string "abdallah_ahmed_habsa"
     t.string "youssefMohamed"
-    t.string "masnour"
+    t.string "masnour_robaa"
     t.string "abdallah_ehab"
     t.string "eyad_emad_hamdy"
     t.string "ahmedadelselim"
