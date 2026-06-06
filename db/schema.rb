4cdf0bf55a87039bd4e6be6a8f00c11b8e1bd227
KSadoun <sadounkhalid@gmail.com>
2026-06-06 15:30:40 +0300

    Add ksadoun column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_20_170639) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_06_122851) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_20_170639) do
     t.string "mahmoud_nabil_number_one"
     t.string "mahmoud_ismail"
     t.string "mahmoud_ismail_elfiky"
+    t.string "ksadoun"
   end
 
   create_table "students", force: :cascade do |t|
