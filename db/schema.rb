beda18952b0e28a218f8912baabef8455252b5dd
Ramadan-Elgamal <ramadanbotsher@gmail.com>
2026-06-06 15:45:52 +0300

    Add ramadan column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_06_122851) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_06_124440) do
   create_table "posts", force: :cascade do |t|
     t.string "MostafaMohamed_Gamgoumm"
     t.string "omar_Gamgoum"
@@ -96,8 +96,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_06_122851) do
     t.string "mahmoud_nabil_number_one"
     t.string "mahmoud_ismail"
     t.string "mahmoud_ismail_elfiky"
-    t.string "ksadoun"
     t.string "shadow_garden"
+    t.string "ksadoun"
+    t.string "ramadan"
   end
 
   create_table "students", force: :cascade do |t|
