831c15fee327a7a717cf897cbaa36a903cd69da8
ahmedsamirkhalaf <ahmedsamirkhalaf671@gmail.com>
2026-06-06 23:22:08 +0300

    Rename amir_mawla_3 to amir_mawla_3_war

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_06_232018) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_06_232019) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmedwagih0"
     t.string "ahmedwagih1"
@@ -116,7 +116,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_06_232018) do
     t.string "ahmedwagih99"
     t.string "ahmedwagih100"
     t.string "abdo5_war"
-    t.string "amir_mawla_3"
+    t.string "amir_mawla_3_war"
     t.string "abdo"
     t.string "ahmed"
   end
