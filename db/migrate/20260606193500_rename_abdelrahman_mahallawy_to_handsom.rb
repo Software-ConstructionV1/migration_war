35a69caf5913b9e6c4e8db73cd370031421a3730
ahmewagih <ahmedwagih087@gmail.com>
2026-06-06 19:35:54 +0300

    3baas

--- db/migrate/20260606193500_rename_abdelrahman_mahallawy_to_handsom.rb
+++ db/migrate/20260606193500_rename_abdelrahman_mahallawy_to_handsom.rb
@@ -1,5 +1,5 @@
 class RenameAbdelrahmanMahallawyToHandsom < ActiveRecord::Migration[7.0]
   def change
-    rename_column :posts, :abdelrahman_mahallawy, :handsom
+    rename_column :posts, :3baass, :3baas
   end
 end
