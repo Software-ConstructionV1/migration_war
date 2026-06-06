95a3abc8e5e3dc5cd24df467b2ddc3cba0a8f9a4
mohamedahmed-dev <mohamedahmed010320@gmail.com>
2026-06-06 19:19:20 +0300

    Fix Mahallawy migration

--- db/migrate/20260606160400_add_mahallawy1_to_posts.rb
+++ db/migrate/20260606160400_add_mahallawy1_to_posts.rb
@@ -1,4 +1,4 @@
-class AddAbdelrahmanMahallawyToPosts < ActiveRecord::Migration[7.1]
+class AddMahallawy1ToPosts < ActiveRecord::Migration[7.1]
   def change
     add_column :posts, :abdelrahman_mahallawy, :string
   end
