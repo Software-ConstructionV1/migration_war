9158ebff35504dc745ea2e2f170e2e7307fcfe26
AhmeedFatehy <ahmedfathyahmed55@gmail.com>
2026-06-07 03:36:41 +0300

    Rename ahmedwagih21 to ahmed_fathi21

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_07_005222) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_07_005223) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmedwagih0"
     t.string "ahmed_fathi1"
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_07_005222) do
     t.string "ahmed_fathi18"
     t.string "ahmed_fathi19"
     t.string "ahmed_fathi20"
-    t.string "ahmedwagih21"
+    t.string "ahmed_fathi21"
     t.string "ahmedwagih22"
     t.string "ahmedwagih23"
     t.string "ahmedwagih24"
