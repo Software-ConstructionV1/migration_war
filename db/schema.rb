a3e94cec47990730dbc13e5cdac136044b2977f3
AhmeedFatehy <ahmedfathyahmed55@gmail.com>
2026-06-07 03:42:19 +0300

    Rename ahmedwagih64 to ahmed_fathi64

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_07_005265) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_07_005266) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmedwagih0"
     t.string "ahmed_fathi1"
@@ -78,7 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_07_005265) do
     t.string "ahmed_fathi61"
     t.string "ahmed_fathi62"
     t.string "ahmed_fathi63"
-    t.string "ahmedwagih64"
+    t.string "ahmed_fathi64"
     t.string "ahmedwagih65"
     t.string "ahmedwagih66"
     t.string "ahmedwagih67"
