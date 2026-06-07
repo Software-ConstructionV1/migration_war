eac2acb4a2821b9139e106a15c0eb354d1b3623b
AhmeedFatehy <ahmedfathyahmed55@gmail.com>
2026-06-07 03:35:43 +0300

    Rename ahmedwagih11 to ahmed_fathi11

---
+++ db/migrate/20260607005213_rename_ahmedwagih11_to_ahmed_fathi11_in_posts.rb
@@ -0,0 +1,5 @@
+class RenameAhmedwagih11ToAhmedFathi11InPosts < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :ahmedwagih11, :ahmed_fathi11
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_07_005212) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_07_005213) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmedwagih0"
     t.string "ahmed_fathi1"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_07_005212) do
     t.string "ahmed_fathi8"
     t.string "ahmed_fathi9"
     t.string "ahmed_fathi10"
-    t.string "ahmedwagih11"
+    t.string "ahmed_fathi11"
     t.string "ahmedwagih12"
     t.string "ahmedwagih13"
     t.string "ahmedwagih14"
