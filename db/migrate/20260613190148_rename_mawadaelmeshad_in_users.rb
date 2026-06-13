b4f94ff80efd307fe25ace672fea9a6bdae5e44d
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 19:01:50 +0300

    Rename mawadaelmeshad to ibrahim_eita_send_regards_mawadaelmeshad in users

---
+++ db/migrate/20260613190148_rename_mawadaelmeshad_in_users.rb
@@ -0,0 +1,5 @@
+class RenamemawadaelmeshadToIbrahim_20260613190148 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :mawadaelmeshad, :ibrahim_eita_send_regards_mawadaelmeshad
+  end
+end
