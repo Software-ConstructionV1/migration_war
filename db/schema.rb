638c3c433b0d6f56e793632f0eba81c752d02b6b
Kerollos Ayman <keroayman34@gmail.com>
2026-06-13 15:47:45 +0300

    Add kero_ayman34 column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_150939) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_150940) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -126,10 +126,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_150939) do
     t.string "ahmed_faheem"
     t.string "islam_khaled_war"
     t.string "islam_khaled"
-    t.string "ahmed_wael"
+    t.string "mohamedHalawa"
     t.string "noorelmobashar_nickname"
     t.string "mohamed"
-    t.string "mohamedHalawa"
     t.string "karim_muhammad"
     t.string "keroraed"
   end
@@ -178,10 +177,11 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_150939) do
     t.string "mohamedabdelshakor"
     t.string "mostafa_abdelmajied"
     t.string "omar_tarek"
-    t.string "mohamed_reda_mohamed"
     t.string "karim_kadry"
+    t.string "mohamed_reda_mohamed"
     t.string "amr_mohamed_shoukry"
     t.string "ahmed_wael"
+    t.string "kero_ayman34"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
