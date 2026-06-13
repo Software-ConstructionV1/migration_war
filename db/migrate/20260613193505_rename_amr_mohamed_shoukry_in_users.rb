bfc0f2d1a20fab27832446982b40f2f2a8ab44ad
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 19:35:06 +0300

    Rename amr_mohamed_shoukry to ibrahim_eita_send_regards_amr_mohamed_shoukry in users

---
+++ db/migrate/20260613193505_rename_amr_mohamed_shoukry_in_users.rb
@@ -0,0 +1,5 @@
+class Renameamr_mohamed_shoukryToIbrahim_20260613193505 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :amr_mohamed_shoukry, :ibrahim_eita_send_regards_amr_mohamed_shoukry
+  end
+end
