1f6c393a97e34765a29ef2acf176db5044baa128
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 19:49:11 +0300

    war: rename ahmed_mohammed_mostafa

---
+++ db/migrate/20260613194910_rename_ahmed_mohammed_mostafa_in_users.rb
@@ -0,0 +1,5 @@
+class Rename20260613194910 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :ahmed_mohammed_mostafa, :ibrahim_eita_send_regards_ahmed_mohammed_mostafa
+  end
+end
