ada47dc00c8b92a715eddc3bc7d8b128c926b76a
ahmed-elemam7 <ahmed.elemam.elazouni@gmail.com>
2026-06-13 13:51:30 +0300

    Add ahmed_ibrahim_elemam column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_104500) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_104852) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -161,8 +161,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_104500) do
     t.string "sohyla"
     t.string "mahmoud"
     t.string "khaled_elmorse"
-    t.string "mawadaelmeshad"
     t.string "shaza_hamdy"
+    t.string "mawadaelmeshad"
+    t.string "ahmed_ibrahim_elemam"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
