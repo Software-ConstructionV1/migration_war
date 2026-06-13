ace0f2bd543f7c78d6cae9303fea19587e058fe9
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 19:01:22 +0300

    Rename Mohamed_Hany_khaldoun to ibrahim_eita_send_regards_Mohamed_Hany_khaldoun in users

---
+++ db/migrate/20260613190119_rename_Mohamed_Hany_khaldoun_in_users.rb
@@ -0,0 +1,5 @@
+class RenameMohamed_Hany_khaldounToIbrahim_20260613190119 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :Mohamed_Hany_khaldoun, :ibrahim_eita_send_regards_Mohamed_Hany_khaldoun
+  end
+end
