cb098926dbd1734c0d31f832071f62b86e421284
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 19:34:00 +0300

    Rename mohamed_tarek_shaza_back to ibrahim_eita_send_regards_mohamed_tarek_shaza_back in users

---
+++ db/migrate/20260613193359_rename_mohamed_tarek_shaza_back_in_users.rb
@@ -0,0 +1,5 @@
+class Renamemohamed_tarek_shaza_backToIbrahim_20260613193359 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :mohamed_tarek_shaza_back, :ibrahim_eita_send_regards_mohamed_tarek_shaza_back
+  end
+end
