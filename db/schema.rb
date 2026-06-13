1e07795dff7e1554bb21801ea9976422a27ccc38
Kerolos Raed <keroraed123@gmail.com>
2026-06-13 15:25:11 +0300

    add keroraed column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_145206) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_150939) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -126,15 +126,12 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_145206) do
     t.string "ahmed_faheem"
     t.string "islam_khaled_war"
     t.string "islam_khaled"
-
     t.string "ahmed_wael"
-
     t.string "noorelmobashar_nickname"
-
-
     t.string "mohamed"
-
-
+    t.string "mohamedHalawa"
+    t.string "karim_muhammad"
+    t.string "keroraed"
   end
 
   create_table "students", force: :cascade do |t|
