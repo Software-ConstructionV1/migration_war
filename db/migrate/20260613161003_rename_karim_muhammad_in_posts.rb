4ea26e32ce7d3afb695c80da7170a1954f99eb2f
Mohamed Waleed <mw99746@gmail.com>
2026-06-13 16:29:19 +0300

    Rename karim_muhammad column in posts

---
+++ db/migrate/20260613161003_rename_karim_muhammad_in_posts.rb
@@ -0,0 +1,5 @@
+class RenameKarimMuhammadInPosts < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :karim_muhammad, :karim_muhammad_renamed_by_waleed if column_exists?(:posts, :karim_muhammad)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_161002) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_161003) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -125,12 +125,12 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_161002) do
     t.string "abdelhamid"
     t.string "ahmed_faheem"
     t.string "islam_khaled_war"
-    t.string "deiaa"
     t.string "islam_khaled"
+    t.string "deiaa"
     t.string "mohamedHalawa"
     t.string "noorelmobashar_nickname"
     t.string "mohamed"
-    t.string "karim_muhammad"
+    t.string "karim_muhammad_renamed_by_waleed"
     t.string "keroraed"
     t.string "kerolosnashat"
     t.string "mohamedwaleed"
