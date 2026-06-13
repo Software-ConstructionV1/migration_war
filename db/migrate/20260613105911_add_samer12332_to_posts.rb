fccce4ad1660d8688105990e1d4a023f1b562f58
samer <sameryousry99@gmail.com>
2026-06-13 10:59:52 +0000

    Add samer12332 to posts

---
+++ db/migrate/20260613105911_add_samer12332_to_posts.rb
@@ -0,0 +1,5 @@
+class AddSamer12332ToPosts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :posts, :samer12332, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_105307) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_105911) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -123,6 +123,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_105307) do
     t.string "rana_hany"
     t.string "AmiraElsa3id"
     t.string "abdelhamid"
+    t.string "samer12332"
   end
 
   create_table "students", force: :cascade do |t|
