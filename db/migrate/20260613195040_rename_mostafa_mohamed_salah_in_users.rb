951a860483af08d547a6a3e8e49156427ed3f3f2
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 19:50:43 +0300

    war: rename mostafa_mohamed_salah

---
+++ db/migrate/20260613195040_rename_mostafa_mohamed_salah_in_users.rb
@@ -0,0 +1,5 @@
+class Rename20260613195040 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :mostafa_mohamed_salah, :ibrahim_eita_send_regards_mostafa_mohamed_salah
+  end
+end
