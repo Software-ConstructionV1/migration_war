d3fe59759bad3a77026f34aaa290b1f31fa0a28d
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 20:05:21 +0300

    war: rename Mohamed_Hany_zagazigBranchEltop

---
+++ db/migrate/20260613200521_rename_Mohamed_Hany_zagazigBranchEltop_in_users.rb
@@ -0,0 +1,5 @@
+class Rename20260613200521 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :Mohamed_Hany_zagazigBranchEltop, :ibrahim_eita_send_regards_Mohamed_Hany_zagazigBranchEltop
+  end
+end
