ba4c186b5a3601b2f90054029ea86ecd47fca280
AhmedMohO <ahmam2004@gmail.com>
2026-06-13 14:05:41 +0300

    Add ahmed_mohammed_mostafa column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_105911) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_110034) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -122,9 +122,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_105911) do
     t.string "youssef2"
     t.string "rana_hany"
     t.string "AmiraElsa3id"
-    t.string "ahmed_faheem"
     t.string "abdelhamid"
     t.string "samer12332"
+    t.string "ahmed_faheem"
   end
 
   create_table "students", force: :cascade do |t|
@@ -167,6 +167,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_105911) do
     t.string "shaza_hamdy"
     t.string "mawadaelmeshad"
     t.string "ahmed_ibrahim_elemam"
+    t.string "ahmed_mohammed_mostafa"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
