93e42e71ca1b24ca1323e23e6262149324f468e7
Kerollos Ayman <keroayman34@gmail.com>
2026-06-13 15:56:17 +0300

    Rename karim_kadry column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_13_150940) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_13_150941) do
   create_table "posts", force: :cascade do |t|
     t.string "ahmed_fathi0"
     t.string "ahmed_fathi1"
@@ -179,7 +179,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_150940) do
     t.string "mohamedabdelshakor"
     t.string "mostafa_abdelmajied"
     t.string "omar_tarek"
-    t.string "karim_kadry"
+    t.string "karim_kadry_war"
     t.string "mohamed_reda_mohamed"
     t.string "amr_mohamed_shoukry"
     t.string "ahmed_wael"
