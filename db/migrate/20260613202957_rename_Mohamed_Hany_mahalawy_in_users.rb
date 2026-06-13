91d657732345038dab70186a56edd40e1641453d
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 20:29:57 +0300

    war: rename Mohamed_Hany_mahalawy

---
+++ db/migrate/20260613202957_rename_Mohamed_Hany_mahalawy_in_users.rb
@@ -0,0 +1,5 @@
+class Rename20260613202957 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :Mohamed_Hany_mahalawy, :ibrahim_eita_send_regards_Mohamed_Hany_mahalawy
+  end
+end
