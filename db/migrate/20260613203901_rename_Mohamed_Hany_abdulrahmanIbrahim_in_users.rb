fad69aa9335aa29ac2bb017b6a58688a02c9d6f8
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-13 20:39:04 +0300

    war: rename Mohamed_Hany_abdulrahmanIbrahim

---
+++ db/migrate/20260613203901_rename_Mohamed_Hany_abdulrahmanIbrahim_in_users.rb
@@ -0,0 +1,5 @@
+class Rename20260613203901 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :Mohamed_Hany_abdulrahmanIbrahim, :ibrahim_eita_send_regards_Mohamed_Hany_abdulrahmanIbrahim
+  end
+end
