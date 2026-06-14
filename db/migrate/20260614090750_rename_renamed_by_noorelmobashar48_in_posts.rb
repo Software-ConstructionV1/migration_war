cf646793d57eed4096c842a0c57e89610c53eb67
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-06-14 09:07:51 +0300

    war: rename renamed_by_noorelmobashar48

---
+++ db/migrate/20260614090750_rename_renamed_by_noorelmobashar48_in_posts.rb
@@ -0,0 +1,5 @@
+class Rename20260614090750 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :renamed_by_noorelmobashar48, :ibrahim_eita_send_regards_renamed_by_noorelmobashar48
+  end
+end
