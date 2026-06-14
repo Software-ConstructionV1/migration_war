681d1d3f67c5bc67bb055a22b4c77e1ab8f8b48a
noorelmobashar <nooralmobasher1@gmail.com>
2026-06-14 04:49:30 +0300

    Rename posts.ahmed_fathi33 to renamed_by_noorelmobashar33

---
+++ db/migrate/20260614014930_rename_to_noorelmobashar33_in_posts.rb
@@ -0,0 +1,5 @@
+class RenameToNoorelmobashar33InPosts < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :ahmed_fathi33, :renamed_by_noorelmobashar33
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_13_094344) do
     t.string "renamed_by_noorelmobashar30"
     t.string "renamed_by_noorelmobashar31"
     t.string "renamed_by_noorelmobashar32"
-    t.string "ahmed_fathi33"
+    t.string "renamed_by_noorelmobashar33"
     t.string "ahmed_fathi34"
     t.string "ahmed_fathi35"
     t.string "ahmed_fathi36"
