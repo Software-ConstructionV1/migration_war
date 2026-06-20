61327acd4c1bbca12b2f28389046f42033e48e44
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-06-20 19:31:41 +0300

    Add ebrahimmostafa133 column to posts

---
+++ db/migrate/20260620185401_add_ebrahimmostafa133_to_posts.rb
@@ -0,0 +1,5 @@
+class AddEbrahimmostafa133ToPosts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :posts, :ebrahimmostafa133, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_20_185400) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_20_185401) do
   create_table "posts", force: :cascade do |t|
     t.string "ibrahim_eita_send_regards_ahmed_fathi0"
     t.string "ibrahim_eita_send_regards_ahmed_fathi1"
@@ -158,6 +158,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_20_185400) do
     t.string "killed_by_hashim"
     t.string "khaleddd11"
     t.string "karim_ibrahim"
+    t.string "ebrahimmostafa133"
   end
 
   create_table "students", force: :cascade do |t|
