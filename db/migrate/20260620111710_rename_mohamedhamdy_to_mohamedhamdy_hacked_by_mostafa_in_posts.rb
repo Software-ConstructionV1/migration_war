9259f01f53974481172f66047015408b54317f0f
Mostafa-Khalifaa <mostafa.khalifa.2000.mk@gmail.com>
2026-06-20 14:18:48 +0300

    Rename mohamedhamdy to mohamedhamdy_hacked_by_mostafa in posts

---
+++ db/migrate/20260620111710_rename_mohamedhamdy_to_mohamedhamdy_hacked_by_mostafa_in_posts.rb
@@ -0,0 +1,5 @@
+class RenameMohamedhamdyToMohamedhamdyHackedByMostafaInPosts < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :mohamedhamdy, :mohamedhamdy_hacked_by_mostafa
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_20_105452) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_20_111710) do
   create_table "posts", force: :cascade do |t|
     t.string "ibrahim_eita_send_regards_ahmed_fathi0"
     t.string "ibrahim_eita_send_regards_ahmed_fathi1"
@@ -149,7 +149,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_20_105452) do
     t.string "andrew"
     t.string "john"
     t.string "mostafa_khalifa"
-    t.string "mohamedhamdy"
+    t.string "mohamedhamdy_hacked_by_mostafa"
   end
 
   create_table "students", force: :cascade do |t|
