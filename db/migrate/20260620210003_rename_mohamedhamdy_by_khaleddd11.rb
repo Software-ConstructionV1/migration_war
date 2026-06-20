1069336460e7f9d6c72125044eaf592a525a3200
Khaleddd11 <161937385+Khaleddd11@users.noreply.github.com>
2026-06-20 21:09:09 +0300

    Rename mohamedhamdy column by khaleddd11

---
+++ db/migrate/20260620210003_rename_mohamedhamdy_by_khaleddd11.rb
@@ -0,0 +1,5 @@
+class RenameMohamedhamdyByKhaleddd11 < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :mohamedhamdy_hacked_by_mostafa, :mohamedhamdy_hacked_by_khaleddd11
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_20_210002) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_20_210003) do
   create_table "posts", force: :cascade do |t|
     t.string "ibrahim_eita_send_regards_ahmed_fathi0"
     t.string "ibrahim_eita_send_regards_ahmed_fathi1"
@@ -149,7 +149,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_20_210002) do
     t.string "moabdelhaqySendshisregardstohasminAndAndrew"
     t.string "john_hacked_by_khaleddd11"
     t.string "khalifael_benzoo_hacked_by_khaleddd11"
-    t.string "mohamedhamdy_hacked_by_mostafa"
+    t.string "mohamedhamdy_hacked_by_khaleddd11"
     t.string "zeyad_shahin_renamed_by_rana"
     t.string "mohamedabdelhaq_renamed_by_rana"
     t.string "mohamed_khalil"
