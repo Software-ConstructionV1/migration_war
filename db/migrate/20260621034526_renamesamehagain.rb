3cb362f2fe3b6f4f849c9335e2f43752e1187727
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-06-21 06:48:02 +0300

    Mo the head of the Table

---
+++ db/migrate/20260621034526_renamesamehagain.rb
@@ -0,0 +1,5 @@
+class Renamesamehagain < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :posts, :moabdelhaqySendshisregardstohasminAndAndrew_conquered_by_mohamed_sameh , :MO_conqueredSameh_Hashim_Andrewtate
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_21_034150) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_21_034526) do
   create_table "posts", force: :cascade do |t|
     t.string "ibrahim_eita_send_regards_ahmed_fathi0"
     t.string "ibrahim_eita_send_regards_ahmed_fathi1"
@@ -146,7 +146,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_21_034150) do
     t.string "mo_ali"
     t.string "Torl_Ali"
     t.string "nourhan"
-    t.string "moabdelhaqySendshisregardstohasminAndAndrew_conquered_by_mohamed_sameh"
+    t.string "MO_conqueredSameh_Hashim_Andrewtate"
     t.string "JAWWWWWWWWWWWWWWWWWWWWWWWWWWWN"
     t.string "EhYabaByKhlil_isForAbdelhaqONLYYY"
     t.string "hamdonlostAgainstAbdelhaq"
